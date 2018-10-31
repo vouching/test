94fa7abab0164e334acce09f7bbf8df789db640b
vouching <v_ned@mail.ru>
2018-10-31 07:52:24 +0300

    ne nau

--- test.cpp
+++ test.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
 int main()
 {
-	long long int a;
-	a=3;
+	stack <int> s;
+	s.push(5);
+	s.pop();
+	s.push(0);
+	int a=s.top();
 	cout<<a;
 	
 }
