1e4355fc7f5834f0370876df67785b1dcb965b8b
vouching <v_ned@mail.ru>
2018-11-02 09:59:46 +0300

    another test

--- vv.cpp
+++ vv.cpp
@@ -7,12 +7,25 @@
 using namespace std;
 int main()
 {
-	int h,m;
-	char g,d;
-	cin>>h>>g>>m>>d;
-	if(d=='a')
-	{h=h;}
-	else
-	{h=h+12;}
-	cout<<h<<":"<<m;
+	int a,b;
+	char c,o;
+	cin>>a>>c;
+	int i=0;
+	while(3==i)
+	{
+		
+	cin>>o;
+	i=1;
+	cin>>b;
+	i=2;
+	cin>>c;
+	i=3;
+	if(o=='+')
+	a=a+b;
+	if(o=='-')
+	a=a-b;
+	}	
+	cout<<a;
+	
+	
 }
