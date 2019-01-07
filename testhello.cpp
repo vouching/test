0bd4c69ba2a66ff0a0885e3277a153599c46f637
Katrinnfoxx <v_ned@mail.ru>
2019-01-07 16:56:08 +0300

    start poker

--- testhello.cpp
+++ testhello.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
-
+#include <vector>
+#include <map> 
 using namespace std;
 int main()
 {
-    cout<<"Hello world!!!";
+    vector <int> cards;
+    for (int i=0;i<=4;i++)
+    {
+		int a;
+        cin>>a;
+        cards.push_back(a);
+    }
+    int x=cards[0];
+    if((cards[1]==x)&&(cards[2]==x)&&(cards[3]==x)&&(cards[4]==x))
+    {
+        cout<<"impossible";
+    }
+    map <int,vector<int> > comb;
+    for(int i=0;i<=4;i++)
+    {
+        comb[cards[i]].push_back(1);
+    }
 }
